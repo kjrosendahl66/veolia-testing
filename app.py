bfedc6eecd5132e32294040a41ade4e6df577493
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2025-02-24 21:12:33 -0600

    download markdown summary as docx

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ import tempfile
 import json
 import re
 import pymupdf
-from markdown_pdf import MarkdownPdf
-from gemini_client import create_client, summarize_cim 
+import pypandoc
+from gemini_client import create_client, summarize_cim, format_summary_as_markdown
 from get_access_token import get_access_token
 from secure_gpt_api import chat_with_api
 from reference_display import render_files
+import pypandoc
 
 # Upload file to GCS
 def upload_blob(bucket_name, destination_blob_name, file):
@@ -23,15 +24,15 @@ def upload_blob(bucket_name, destination_blob_name, file):
 
     return f"gs://{bucket_name}/{destination_blob_name}"
 
+def render_markdown(text):
+    text = text.replace("$", "\$").replace("<br>", "")
+    return text
 
 st.set_page_config(layout="wide")
 st.title("V-Accelerate: Memo Generation Tool") 
 st.write(" \n  ")
 st.write(" \n  ")
 
-# col1, spacer, col2 = st.columns([1, 0.1, 1])
-
-float_init()
 load_dotenv()
 
 project_id = os.getenv("PROJECT_ID")
@@ -52,9 +53,6 @@ if "temp_dir" not in st.session_state:
 if "docs" not in st.session_state:
     st.session_state.docs = {}
 
-
-
-# with col1: 
 
 model_option = st.selectbox(label="Select the model to use", options=("gemini-1.5-pro", "gemini-2.0-flash", "Secure GPT"))
 uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
@@ -95,23 +93,40 @@ if len(uploaded_files) > 1:
                 gemini_client = create_client(project_id, location, model_name=model_option)
 
                 summary_from_gemini = summarize_cim(gemini_client, st.session_state.files)
+                display_summary = format_summary_as_markdown(gemini_client, summary_from_gemini)
+
                 st.session_state.summary = summary_from_gemini 
+                st.session_state.display_summary = display_summary
 
             elif model_option == "Secure GPT": 
                 st.session_state.summary= "Secure GPT not implemented yet. "
 
-    if st.session_state.summary: 
-        st.write(st.session_state.summary)
+    if st.session_state.display_summary: 
 
-            
+        st.markdown(render_markdown(st.session_state.display_summary), unsafe_allow_html=True)
+        st.text(st.session_state.display_summary)
+
+        markdown_filename = "summary.md"
+        output_filename = "summary.docx"
+        markdown_path = os.path.join(st.session_state.temp_dir, markdown_filename)
+        output_path = os.path.join(st.session_state.temp_dir, output_filename)
+
+        with open(markdown_path, "w") as f:
+            f.write(st.session_state.summary)
+
+        pypandoc.convert_file(markdown_path, 'docx', outputfile=output_path)
+        
+        download = st.download_button(
+            label="Download summary as a docx!",
+            data=open(output_path, "rb").read(),
+            file_name=output_filename,
+        )
+    
 else: 
     st.write("Please upload at least two files to get started.")
 
-
 with st.sidebar: 
     container = st.container(border = True)
-
-    # st.text("  \n  \n  \n  ")
     with container:
         render_files()
 
