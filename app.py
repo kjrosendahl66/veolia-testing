54ea814808237f5b6d6dd2e61d956394541c667e
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2025-02-25 09:28:47 -0600

    documentation

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import streamlit as st
 from google.cloud import storage
-import vertexai 
 import os 
 from datetime import datetime 
 from dotenv import load_dotenv
 import tempfile
-import json
-import re
 import pymupdf
 import pypandoc
 from gemini_client import create_client, summarize_cim, format_summary_as_markdown
 from get_access_token import get_access_token
-from secure_gpt_api import chat_with_api
 from reference_display import render_files
-import pypandoc
 
 # Upload file to GCS
 def upload_blob(bucket_name, destination_blob_name, file):
@@ -24,28 +19,29 @@ def upload_blob(bucket_name, destination_blob_name, file):
 
     return f"gs://{bucket_name}/{destination_blob_name}"
 
+# Render markdown for streamlit 
 def render_markdown(text):
     text = text.replace("$", "\$").replace("<br>", "")
     return text
 
+# Set configuration and title 
 st.set_page_config(layout="wide")
 st.title("V-Accelerate: Memo Generation Tool") 
 st.write(" \n  ")
 st.write(" \n  ")
 
+# Load environment variables
 load_dotenv()
-
 project_id = os.getenv("PROJECT_ID")
 location = os.getenv("LOCATION")
 bucket_name = os.getenv("BUCKET_NAME")
 
-# API URL
+# Get access token for SecureGPT 
 token_url = "https://api.veolia.com/security/v2/oauth/token"
 api_url = 'https://api.veolia.com/llm/veoliasecuregpt/v1/answer'
-
-# Get access token and init Vertex
 access_token = get_access_token(token_url, api_url)
 
+# Initialize session state
 if "files" not in st.session_state: 
     st.session_state.files = {}
 if "temp_dir" not in st.session_state:
@@ -53,69 +49,85 @@ if "temp_dir" not in st.session_state:
 if "docs" not in st.session_state:
     st.session_state.docs = {}
 
-
+# Model selection and file input
 model_option = st.selectbox(label="Select the model to use", options=("gemini-1.5-pro", "gemini-2.0-flash", "Secure GPT"))
 uploaded_files = st.file_uploader("Upload PDF files", type=["pdf"], accept_multiple_files=True)
 
 if len(uploaded_files) > 1: 
+
+    # Process input files
     if not st.session_state.files:
 
         with st.spinner("Processing files..."):
 
+            # Create a timestamp for the files
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
             
             for file in uploaded_files:
                 # Upload the files to GCS for the LLM
-                uploaded_location = upload_blob(bucket_name, f"files/{timestamp}/{file.name}", file)
-                # Save the files to the temp directory for rendering
+                gcs_location = upload_blob(bucket_name, f"files/{timestamp}/{file.name}", file)
+                # Save the files to a temp directory
                 path = os.path.join(st.session_state.temp_dir, file.name)
                 with open(path, "wb") as f:
                     f.write(file.getvalue())
 
-                # Save the file locations
+                # Save locations of the files to the session state
                 st.session_state.files.update(
                     {file.name: 
                         {"local_file_location": path, 
-                        "gcs_file_location": uploaded_location
+                        "gcs_file_location": gcs_location
                         }
                     }
                 )
 
+                # Open and save the rendered PDF files as pymupdf docs to the session state
                 st.session_state.docs[file.name] = pymupdf.open(path)
 
         st.toast('Files processed succesfully!', icon='🎉')
 
+    # Generate summary 
     if len(st.session_state.files) > 1 and "summary" not in st.session_state:
+
         with st.spinner("Generating summary..."):
 
+            # Generate summary using Gemini 
             if model_option.startswith("gemini"): 
                 
+                # Create a client
                 gemini_client = create_client(project_id, location, model_name=model_option)
 
+                # Generate main summary
                 summary_from_gemini = summarize_cim(gemini_client, st.session_state.files)
+                # Generate a summary for markdown display in streamlit
                 display_summary = format_summary_as_markdown(gemini_client, summary_from_gemini)
 
+                # Save both summaries to the session state
                 st.session_state.summary = summary_from_gemini 
                 st.session_state.display_summary = display_summary
 
             elif model_option == "Secure GPT": 
                 st.session_state.summary= "Secure GPT not implemented yet. "
 
+    #  Display markdown summary 
     if st.session_state.display_summary: 
 
+        # Render the markdown summary and display in streamlit 
         st.markdown(render_markdown(st.session_state.display_summary), unsafe_allow_html=True)
-        st.text(st.session_state.display_summary)
-
-        markdown_filename = "summary.md"
+        
+        # Declare variables for downloading the summary as a docx
+        summary_filename = "summary.md"
         output_filename = "summary.docx"
-        markdown_path = os.path.join(st.session_state.temp_dir, markdown_filename)
+        summary_path = os.path.join(st.session_state.temp_dir, summary_filename)
         output_path = os.path.join(st.session_state.temp_dir, output_filename)
 
-        with open(markdown_path, "w") as f:
+        # Save the summary to a local path
+        with open(summary_path, "w") as f:
             f.write(st.session_state.summary)
 
-        pypandoc.convert_file(markdown_path, 'docx', outputfile=output_path)
+        # Convert the summary to a docx file
+        pypandoc.convert_file(summary_path, 'docx', outputfile=output_path)
         
+        # Display a download button for the docx file
         download = st.download_button(
             label="Download summary as a docx!",
             data=open(output_path, "rb").read(),
@@ -125,6 +137,7 @@ if len(uploaded_files) > 1:
 else: 
     st.write("Please upload at least two files to get started.")
 
+# Display the uploaded files in the sidebar
 with st.sidebar: 
     container = st.container(border = True)
     with container:
--- reference_display.py
+++ reference_display.py
@@ -1,55 +1,72 @@
 
 import streamlit as st 
-def display_page(doc):
 
+# Load a document page
+def display_page(doc):
+    # Load the page
     page = doc.load_page(st.session_state.get("current_page",1) - 1)
+    # Display the page
     st.image(page.get_svg_image())
     st.caption(f"Page {st.session_state.get('current_page',1)} of {doc.page_count}")
 
+# Add navigation buttons to the file viewer
 def navigation_buttons(doc):
     col_nav1, col_nav2, col_nav3 = st.columns([1, 2, 1])
 
+    # Button for previous page
     with col_nav1:
+        # Display button for all pages except the first
         if st.session_state.get("current_page",1) > 1:
             st.write("\n")
+            # Update page and rerun session 
             if st.button("Previous Page", key="prev_button"):
                 st.session_state["current_page"] -= 1
                 st.rerun()
 
+    # Select box for jumping to a specific page
     with col_nav2:
         if 1 <= st.session_state.get("current_page",1) <= doc.page_count:
+            # Display all page numbers 
             page_number = st.selectbox("Jump to page:", options=range(1, doc.page_count + 1),
                                         index=st.session_state.get("current_page", 1) - 1,  # Show current page
-                                        key="page_selectbox" # Added key so that this reruns on value change.
+                                        key="page_selectbox" 
             )
+
+            # Jump to the selected page and update page and session 
             if page_number != st.session_state.get("current_page", 1):
                 st.session_state["current_page"] = page_number
                 st.rerun()
 
+    # Button for next page
     with col_nav3:
+      # Display button for all pages except the last
       if st.session_state.get("current_page",1) < doc.page_count:
         st.write("\n")
+        # Update page and rerun session
         if st.button("Next Page", key="next_button"):
             st.session_state["current_page"] += 1
             st.rerun()
 
 def render_files():
+
+    # Display the file viewer with uploaded files 
     if st.session_state.files: 
         file_names = list(st.session_state.files.keys())
-
         file_option = st.selectbox("Select a file to view", options=file_names, index=0)
-        if file_option:
 
-           #Reset the page when file is changed.
+        if file_option:
+           # Load the selected file
             if "last_selected_file" not in st.session_state:
                 st.session_state.last_selected_file = file_option
                 st.session_state.current_file = st.session_state.docs[file_option]
                 st.session_state.current_page = 1
+            # Update the selected file when option is changed
             elif st.session_state.last_selected_file != file_option:
                 st.session_state.last_selected_file = file_option
                 st.session_state.current_file = st.session_state.docs[file_option]
                 st.session_state.current_page = 1
 
+            # Load the document and display the page
             doc = st.session_state.docs[file_option]
             navigation_buttons(doc)
             display_page(doc)
