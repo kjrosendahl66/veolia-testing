dfe20f5e957fb456ac5136e2bb3a9c61719926bd
Mike Blow <michael.blow@66degrees.com>
2025-02-26 19:26:48 -0500

    adding gitignore

--- get_access_token.py
+++ get_access_token.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import oauthlib
+import oauthlib.oauth2.rfc6749.errors
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 from fastapi import HTTPException
@@ -25,7 +25,7 @@ def get_access_token(token_url, api_url):
     try:
         token = oauth.fetch_token(token_url=token_url, client_id=client_id, client_secret=client_secret)
         return token['access_token'] 
-    except oauthlib.oauth2.TokenError as e:
+    except oauthlib.oauth2.rfc6749.errors.OAuth2Error as e:
         logging.error(f"Error fetching access token: {e}")
         raise HTTPException(status_code=500, detail="Error fetching access token")
     except Exception as e:
