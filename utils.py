b5150b7f8da4857fe0f618c20e6172d86072b2a3
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2025-03-05 14:03:35 -0600

    move upload gcs and save to local directory as seperate function

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from google.cloud import storage
-
+from datetime import datetime
+import os 
+import streamlit as st
 
 # Upload file to GCS
 def upload_blob(bucket_name, destination_blob_name, file):
@@ -16,3 +18,30 @@ def upload_blob(bucket_name, destination_blob_name, file):
 def render_markdown(text):
     text = text.replace("\\", "\\\\").replace("$", "\$").replace("<br>", " ")
     return text
+
+
+def upload_gcs_and_save(bucket_name, file, file_type):
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Upload the files to GCS for the LLM
+    gcs_location = upload_blob(
+        bucket_name, f"files/{timestamp}/{file.name}", file
+    )
+    # Save the files to a temp directory
+    path = os.path.join(st.session_state.temp_dir, file.name)
+    with open(path, "wb") as f:
+        f.write(file.getvalue())
+
+    # Save locations of the files to the session state
+    st.session_state.files.update(
+        {
+            file.name: {
+                "file_type": file_type,
+                "local_file_location": path,
+                "gcs_file_location": gcs_location,
+                "mime_type": file.type,
+            }
+        }
+    )
+
+    return path
